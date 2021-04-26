36155834743d034e04f2b89754cac74cbdeb5d04
Kianna <73039627+kbarbarisi@users.noreply.github.com>
2021-04-26 18:37:45 -0400

    Update CollectiveCODE.py

--- CollectiveCODE.py
+++ CollectiveCODE.py
@@ -417,100 +417,103 @@ from PIL import Image
 from PIL import ImageTk
 
 # Create root window
-root = Tk()
-root.title('Brain Teaser - Hacking Word Edition')
-root.geometry("600x400+-1900+100")
+for i in range(3):
+	root = Tk()
+	root.title('Brain Teaser - Hacking Word Edition')
+	root.geometry("600x400+-1900+100")
 
 
-my_label = Label(root, text="", font=("Helvetica", 48))
-my_label.pack(pady=20)
+	my_label = Label(root, text="", font=("Helvetica", 48))
+	my_label.pack(pady=20)
 
 
-def shuffler():
-    # Clear the Hint Label
-    hint_label.config(text='')
+	def shuffler():
+	    # Clear the Hint Label
+	    hint_label.config(text='')
 
-    # Clear the Hint Count
-    global hint_count
-    hint_count = 0
+	    # Clear the Hint Count
+	    global hint_count
+	    hint_count = 0
 
-    # Clear the Answer Box
-    entry_answer.delete(0, END)
+	    # Clear the Answer Box
+	    entry_answer.delete(0, END)
 
-    # Clear the Answer Label
-    answer_label.config(text='')
+	    # Clear the Answer Label
+	    answer_label.config(text='')
 
-    # List of hacking terms
-    hackingterms = ['phishing','malware','ransomware','encryption','denial of service','rootkit','social engineering']
+	    # List of hacking terms
+	    hackingterms = ['phishing','malware','ransomware','encryption','rootkit', 'firewall', 'payload']
 
-    # Pick hacking term from list
-    global word
-    word = choice(hackingterms)
+	    # Pick hacking term from list
+	    global word
+	    word = choice(hackingterms)
 
-    # Break apart word
-    break_word = list(word)
-    shuffle(break_word)
+	    # Break apart word
+	    break_word = list(word)
+	    shuffle(break_word)
 
-    # Turn List into word
-    global shuffled_word
-    shuffled_word = ''
-    for letter in break_word:
-        shuffled_word += letter
+	    # Turn List into word
+	    global shuffled_word
+	    shuffled_word = ''
+	    for letter in break_word:
+		shuffled_word += letter
 
-    # print to screen
-    my_label.config(text=shuffled_word)
-
-
-# Create answer Function
-def answer():
-    if word == entry_answer.get():
-        answer_label.config(text="Correct!!")
-    else:
-        answer_label.config(text="Incorrect!!")
+	    # print to screen
+	    my_label.config(text=shuffled_word)
 
 
-# Create Hint Counter
-global hint_count
-hint_count = 0
+	# Create answer Function
+	def answer():
+	    if word == entry_answer.get():
+		answer_label.config(text="Correct!!")
+		print('Congratulations on unscrambling the word! You may now move onto the next level!\n')
+            	root.destroy()
+	    else:
+		answer_label.config(text="Incorrect!!")
 
 
-# Create Hint Function
-def hint(count):
-    global hint_count
-    hint_count = count
-
-    # length of the chosen word
-    word_length = len(word)
-
-    # Show Hint
-    if count < word_length:
-        hint_label.config(text=f'{hint_label["text"]} {word[count]}')
-        hint_count += 1
+	# Create Hint Counter
+	global hint_count
+	hint_count = 0
 
 
-entry_answer = Entry(root, font=("Helvetica", 24))
-entry_answer.pack(pady=20)
+	# Create Hint Function
+	def hint(count):
+	    global hint_count
+	    hint_count = count
 
-button_frame = Frame(root)
-button_frame.pack(pady=20)
+	    # length of the chosen word
+	    word_length = len(word)
 
-answer_button = Button(button_frame, text="Answer", command=answer)
-answer_button.grid(row=0, column=0, padx=10)
+	    # Show Hint
+	    if count < word_length:
+		hint_label.config(text=f'{hint_label["text"]} {word[count]}')
+		hint_count += 1
 
-my_button = Button(button_frame, text="Pick Another Word", command=shuffler)
-my_button.grid(row=0, column=1, padx=10)
 
-hint_button = Button(button_frame, text="Hint", command=lambda: hint(hint_count))
-hint_button.grid(row=0, column=2, padx=10)
+	entry_answer = Entry(root, font=("Helvetica", 24))
+	entry_answer.pack(pady=20)
 
-answer_label = Label(root, text='', font=("Helvetica", 18))
-answer_label.pack(pady=20)
+	button_frame = Frame(root)
+	button_frame.pack(pady=20)
 
-hint_label = Label(root, text='', font=("Helvetica", 18))
-hint_label.pack(pady=10)
+	answer_button = Button(button_frame, text="Answer", command=answer)
+	answer_button.grid(row=0, column=0, padx=10)
 
-shuffler()
-root.mainloop()
+	my_button = Button(button_frame, text="Pick Another Word", command=shuffler)
+	my_button.grid(row=0, column=1, padx=10)
+
+	hint_button = Button(button_frame, text="Hint", command=lambda: hint(hint_count))
+	hint_button.grid(row=0, column=2, padx=10)
+
+	answer_label = Label(root, text='', font=("Helvetica", 18))
+	answer_label.pack(pady=20)
+
+	hint_label = Label(root, text='', font=("Helvetica", 18))
+	hint_label.pack(pady=10)
+
+	shuffler()
+	root.mainloop()
 
 
 
